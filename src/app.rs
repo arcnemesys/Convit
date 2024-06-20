4a448d4f3287768d45aed565be9ce43895135440
arcnemesys <accelerenzo@proton.me>
2024-06-20 03:13:59 -0400

    default for App & ConventionalCommit, from CommitType and reference for String, from String for CommitType

---
+++ src/app.rs
@@ -0,0 +1,172 @@
+use std::collections::HashMap;
+
+use serde_json::Result;
+
+pub enum CurrentScreen {
+    Main,
+    EditingCommitType,
+    EditingCommitScope,
+    EditingCommitDescription,
+    EditingCommitBody,
+    EditingCommitFooters,
+    Exiting,
+}
+
+pub enum CurrentlyEditing {
+    CommitType,
+    CommitScope,
+    CommitDescription,
+    CommitBody,
+    CommitFooters,
+}
+
+// Users should be able to select from a list of commit types,
+// which will spare us having to mutate a string to match it
+// against the enum, to make sure the commit type input
+// is valid.
+
+#[derive(PartialEq)]
+pub enum CommitType {
+    Fix,
+    Feat,
+    Build,
+    Chore,
+    Ci,
+    Docs,
+    Style,
+    Refactor,
+    Perf,
+    Test,
+}
+
+pub enum CommitFooter {
+    BreakingChange,
+}
+
+pub struct ConventionalCommit {
+    pub commit_type: String,
+    pub scope: Option<String>,
+    pub description: String,
+    pub body: Option<String>,
+    pub footers: Option<Vec<String>>,
+}
+
+impl Default for ConventionalCommit {
+    fn default() -> Self {
+        Self {
+            commit_type: String::new(),
+            scope: None,
+            description: String::new(),
+            body: None,
+            footers: None
+        }
+    }
+}
+
+impl ConventionalCommit {
+    fn new() -> Self {
+        ConventionalCommit::default()
+    }
+}
+pub struct App {
+    pub conventional_commit: ConventionalCommit,
+    pub commit_type_input: String,
+    pub commit_scope: Option<String>,
+    pub commit_description: String,
+    pub commit_body: Option<String>,
+    pub commit_footers: Option<HashMap<CommitFooter, String>>,
+    pub current_screen: CurrentScreen,
+    pub currently_editing: Option<CurrentlyEditing>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            conventional_commit: ConventionalCommit::default(),
+            commit_type_input: String::new(),
+            commit_scope: None,
+            commit_description: String::new(),
+            commit_body: None,
+            commit_footers: None,
+            current_screen: CurrentScreen::Main,
+            currently_editing: None,
+        }
+    }
+}
+
+// pub fn match_commit_type(commit_input:)
+
+
+impl From<CommitType> for String {
+    fn from(value: CommitType) -> Self {
+        let commit_type = match value {
+            CommitType::Fix => String::from("fix"),
+            CommitType::Feat => String::from("feat"),
+            CommitType::Build => String::from("build"),
+            CommitType::Chore => String::from("chore"),
+            CommitType::Ci => String::from("ci"),
+            CommitType::Docs => String::from("docs"),
+            CommitType::Style => String::from("style"),
+            CommitType::Refactor => String::from("refactor"),
+            CommitType::Perf => String::from("perf"),
+            CommitType::Test => String::from("test")
+        };
+
+        commit_type
+    }
+}
+
+impl From<&CommitType> for String {
+    fn from(value: &CommitType) -> Self {
+        let commit_type = match value {
+            CommitType::Fix => String::from("fix"),
+            CommitType::Feat => String::from("feat"),
+            CommitType::Build => String::from("build"),
+            CommitType::Chore => String::from("chore"),
+            CommitType::Ci => String::from("ci"),
+            CommitType::Docs => String::from("docs"),
+            CommitType::Style => String::from("style"),
+            CommitType::Refactor => String::from("refactor"),
+            CommitType::Perf => String::from("perf"),
+            CommitType::Test => String::from("test")
+        };
+
+        commit_type
+    }
+}
+
+impl From<String> for CommitType {
+    fn from(value: String) -> Self {
+        let commit_type = match value.to_lowercase().as_str() {
+            "fix" => CommitType::Fix,
+            "feat" => CommitType::Feat,
+            "build" => CommitType::Build,
+            "chore" => CommitType::Chore,
+            "ci" => CommitType::Ci,
+            "docs" => CommitType::Docs,
+            "style" => CommitType::Style,
+            "refactor" => CommitType::Refactor,
+            "perf" => CommitType::Perf,
+            "test" => CommitType::Test,
+            &_ => unreachable!()
+        };
+
+        commit_type
+        
+    }
+}
+impl App {
+    pub fn new() -> App {
+        App::default()
+    }
+    //  pub fn toggle_edit
+    pub fn save_commit_type(&mut self) {
+        self.conventional_commit.commit_type = self.commit_type_input.clone();
+    }
+
+    pub fn update_commit_type(&mut self, commit_type: &CommitType) {
+        if self.conventional_commit.commit_type != String::from(commit_type) {
+            self.conventional_commit.commit_type = commit_type.into()
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{error::Error, io};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+mod app;
+mod ui;
+use crate::{
+    app::{App, CurrentScreen, CurrentlyEditing},
+};
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) {}
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+
+    Ok(())
+}
+
